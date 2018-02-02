5cd73fa161f9c3bd66bc5d014dfd7c14a9b605e5
1dechampl <35352275+1dechampl@users.noreply.github.com>
2018-02-01 19:10:13 -0500

    uploaded to main page

--- Assignment 2.0/sketch.js
+++ Assignment 2.0/sketch.js
@@ -3,14 +3,17 @@ function setup() {
 }
 
 function draw() {
-	background(0);
+	 var bg = map(mouseX, 0, width, 255, 0);
+	background(bg);
 	strokeWeight(4);
 	stroke(255)
 
 	for (var x = 0; x <= width; x += 50) {
 		for (var y = 0; y <= width; y += 50) {
-	fill(0, random(255), random(255))
-	rect(x, y, 25, 25)
-	}
-	}
+	fill(0, random(255), random(255));
+	rect(x, y, 25, 25);
+        }
+    }
 }
+	
+	

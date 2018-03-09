a836e0300bcc54c437df4f57ba6c509bd2be5076
1dechampl <35352275+1dechampl@users.noreply.github.com>
2018-03-09 10:47:30 -0500

    update

---
+++ Assignment 4.0/sketch.js
@@ -0,0 +1,59 @@
+//    ARTG2260
+//    Louis Dechamps
+//    louis.dechamps99@gmail.com
+//    Assignment Game
+
+var s;
+var scl = 20;
+let score = 0;
+var food;
+
+function setup() {
+  createCanvas(600, 600);
+  s = new Snake();
+  frameRate(10);
+  pickLocation();
+
+}
+
+function pickLocation() {
+  var cols = floor(width/scl);
+  var rows = floor(height/scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
+}
+
+function mousePressed() {
+  s.total++;
+}
+
+function draw() {
+  background(0);
+
+  s.death();
+  s.update();
+  s.show();
+
+  fill(255)
+  score++;
+  text("Score = " + score, 500, 20);
+
+  
+  
+}
+
+
+
+
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
+  } 
+}
